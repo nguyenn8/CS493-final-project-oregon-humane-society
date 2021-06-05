be45823af40fd6c96b10ac245021d1c91cea4fdd
obergad <obergad@oregonstate.edu>
2021-06-05 15:36:41 -0700

    added rate limiting using redis

--- server.js
+++ server.js
@@ -1,15 +1,84 @@
 const express = require("express");
 const morgan = require("morgan");
+const redis = require('redis') ; //Rate Limiting
 const { ApolloServer } = require("apollo-server-express");
 
 const typeDefs = require("./schemas/schema");
 
 const api = require("./api");
 
+
+
 const app = express();
 const port = process.env.PORT || 8000;
 const server = new ApolloServer({ typeDefs });
 server.applyMiddleware({ app });
+
+//=====Rate Limiting Start====
+const redisClient = redis.createClient(
+  process.env.REDIS_PORT || '6397',
+  process.envREDIS_HOST || 'localhost'
+);
+const rateLimitWindowMS = 60000;
+const rateLimitMaxRequests = 5;
+
+function getUserTokenBucket(ip) {
+  return new Promise((resolve, reject) => {
+    redisClient.hgetall(ip, (error, tokenBucket) => {
+      if (err){
+        reject(err);
+      } else if (tokenBucket){
+        tokenBucket.tokens = parseFloat(tokenBucket.tokens);
+        resolve(tokenBucket);
+      } else {
+        resolve({
+          tokens: rateLimitMaxRequests,
+          last: Date.now()
+        });
+      }
+    });
+  });
+}
+
+function saveUserTokenBucket(ip, tokenBucket) {
+  return new Promise((resolve, reject) => {
+    redisClient.hmset(ip, tokenBucket, (err, resp) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function rateLimit(req, res, next) {
+  try {
+    const tokenBucket = await getUserTokenBucket(req.ip);
+    const currentTime = Date.now();
+    const ellapsedTime = currentTime - tokenBucket.last;
+    tokenBucket.tokens += ellapsedTime *
+      (rateLimitMaxRequests / rateLimitWindowMS);
+    tokenBucket.tokens = Math.min(tokenBucket.tokens,
+        rateLimitMaxRequests);
+    tokenBucket.last = currentTime;
+
+    if (tokenBucket.token >= 1) {
+      tokenBucket.tokens -= 1;
+      await saveUserTokenBucket(req.id, tokenBucket);
+      next();
+    } else {
+      res.status(429).send({
+        error: "Too many requests per minute. Please wait a minute."
+      });
+    }
+  } catch (err) {
+    next();
+  }
+}
+
+app.use(rateLimit);
+//=====Rate Limiting End====
 /*
  * Morgan is a popular logger.
  */
