d3d36a0bb7b05c012532cb2eb19aeb6ff9a78dbf
nguyenn8 <nguyenn8@oregonstate.edu>
2021-06-06 17:51:38 -0700

    fixed api/animal.js

--- api/animals.js
+++ api/animals.js
@@ -2,33 +2,66 @@ const router = require("express").Router();
 const { DataSource } = require("apollo-datasource");
 const { getDbReference } = require("../lib/mongo");
 const { ObjectId } = require("mongodb");
+const { AnimalSchema } = require("../models/animal");
 
 class AnimalsAPI extends DataSource {
   constructor() {
-    super();
-    this.db = getDbReference();
+
+
   }
 
   async getAllAnimals() {
-    const collection = this.db.collection('animals');
+    const collection = this.db.collection("animals");
     const results = await collection.find({}).toArray();
     return results;
   }
 
   async getAnimalById({ animalId }) {
     console.log(animalId);
-    const collection = this.db.collection('animals');
-    const results = await collection.find({ _id: ObjectId(animalId) }).toArray();
+    const collection = this.db.collection("animals");
+    const results = await collection
+       .find({ _id: ObjectId(animalId) })
+       .toArray();
     console.log(results);
     return results[0];
   }
 
-  getAnimalsByLocationId({ locationId }) {
-    return this.data.filter((animal) => animal.locationId === locationId);
-  }
+  async getAnimalsByLocationId({ locationId }) {
+     console.log(locationId);
+     const collection = this.db.collection("animals");
+     const results = await collection
+       .find({ locationId: ObjectId(locationId) })
+       .toArray();
+     console.log(results);
+     return results[0];
+   }
+
+   async getAnimalsByBreed({ breed }) {
+     console.log(breed);
+     const collection = this.db.collection("animals");
+     const results = await collection.find({ breed: breed }).toArray();
+     console.log(results);
+     return results[0];
+   }
+
+   async addNewAnimal({ animal }) {
+     animal = extractValidFields(animal, AnimalSchema);
+     const collection = this.db.collection("animals");
+     const result = await collection.insertOne(animal);
+     return result.insertedId;
+ }
+
+  async updateAnimalInfo({}) {
+    const collection = this.db.collection("animals");
+ }
+
 
   getAnimalsByBreed({ breed }) {
     return this.data.filter((animal) => animal.breed === breed);
   }
+
+  async deleteAnimalInfo({}) {
+   const collection = this.db.collection("animals");
+ }
 }
 module.exports = AnimalsAPI;
