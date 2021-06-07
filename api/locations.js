0606d58d31310a0fb51ed420ca5e34e0d1127a93
nguyenn8 <nguyenn8@oregonstate.edu>
2021-06-06 23:52:54 -0700

    added all endpoints for location

--- api/locations.js
+++ api/locations.js
@@ -1,41 +1,146 @@
 const router = require("express").Router();
-const { DataSource } = require("apollo-datasource");
 const { getDbReference } = require("../lib/mongo");
 const { ObjectId } = require("mongodb");
+const { validateUser } = require("../lib/validation");
+const { requireAuthentication } = require('../lib/auth');
+const { validateAgainstSchema } = require('../lib/validation');
 
-class LocationAPI extends DataSource {
-  constructor() {
-    super();
-    this.db = getDbReference();
-  }
+const {
+  LocationSchema,
+  getLocationsPage,
+  insertNewLocation,
+  replaceLocationById,
+  deleteLocationById,
+  getLocationById
+} = require('../models/location');
 
-  async getAllLocations() {
-    const collection = this.db.collection("locations");
-    const results = await collection.find({}).toArray();
-    return results;
+/*
+ * Route to get information about all locations.
+ */
+ router.get('/', async (req, res) => {
+  try {
+    const locationsPage = await getLocationsPage(parseInt(req.query.page) || 1);
+    console.log("---locationsPage", locationsPage);
+    res.status(200).send(locationsPage);
+  } catch (err) {
+    console.error("  -- error:", err);
+    res.status(500).send({
+      err: "Error fetching locations page from DB.  Try again later."
+    });
   }
+});
 
-  async getLocationById({ locationId }) {
-    console.log(locationId);
-    const collection = this.db.collection("locations");
-    const results = await collection
-      .find({ _id: ObjectId(locationId) })
-      .toArray();
-    console.log(results);
-    return results[0];
+/*
+ * Route to create a new location if administrator
+ */
+router.post("/", async (req, res, next) => {
+  console.log("  -- req.body:", req.body);
+  if (validateAgainstSchema(req.body, LocationSchema)) {
+    try {
+      const id = await insertNewLocation(req.body);
+      res.status(201).send({
+        _id: id,
+      });
+    } catch (err) {
+      console.error("  -- Error:", err);
+      res.status(500).send({
+        error: "Error inserting new location.  Try again later.",
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body does not contain a valid User.",
+    });
   }
+});
 
-  async updateLocationById({ locationId }) {
-    console.log(locationId);
-    const collection = this.db.collection("locations");
-  }
+ /*
+ * Route to fetch info about a specific location including adoptable animals in this location
+ */
+ router.get('/:id', async (req, res, next) => {
+   try {
+     const location = await getLocationById(req.params.id);
+     if (location) {
+       res.status(200).send(location);
+     } else {
+       next();
+     }
+   } catch (err) {
+     console.error(err);
+     res.status(500).send({
+       error: "Unable to fetch location.  Please try again later."
+     });
+   }
+ });
 
-  async addNewLocation({}) {
-    const collection = this.db.collection("locations");
-  }
+ /*
+ * Route to update a location.
+ * Update information about a specific location if administrator
+ */
+ router.put('/:id', async (req, res, next) => {
+ console.log("req.params.id", req.params.id);
+ const location = await getLocationById(req.params.id);
+ console.log("location._id", location._id);
+ if( (parseInt(req.params.id)) !==  (parseInt(location._id)) ){
+   res.status(403).send({
+     error: "Unauthorized to access the specified resource"
+   });
+ } else {
+   if (validateAgainstSchema(req.body, LocationSchema)) {
+     try {
+       const updateSuccessful = await replaceLocationById(req.params.id, req.body);
+       if (updateSuccessful){
+         res.status(200).send({
+           links: {
+             location: `/photos/${(parseInt(location._id))}`
+           }
+         });
+       }
+       else {
+         next();
+       }
+     } catch (err) {
+       console.error(err);
+       res.status(500).send({
+        error: "Unable to update photo.  Please try again later."
+      });
+     }
+   }
+    else {
+     res.status(400).send({
+       error: "Request body is not a valid location object."
+     });
+   }
+ }
+ });
 
-  async deleteLocationById({}) {
-    const collection = this.db.collection("locations");
+
+ /*
+ * Route to delete a location.
+ * Delete a location from the shelter database
+ */
+ router.delete('/:id', async (req, res, next) => {
+   console.log("req.params.id", req.params.id);
+   const location = await getLocationById(req.params.id);
+  if((parseInt(req.params.id)) !==  (parseInt(location._id))){
+    res.status(403).send({
+      error: "Unauthorized to access the specified resource"
+    });
+  } else {
+    try {
+      const deleteSuccessful = await deleteLocationById(req.params.id);
+      if (deleteSuccessful) {
+        res.status(204).end();
+      } else {
+        next();
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        error: "Unable to delete location.  Please try again later."
+      });
+    }
   }
-}
-module.exports = LocationAPI;
+});
+
+module.exports = router;
--- models/location.js
+++ models/location.js
@@ -3,12 +3,101 @@
  */
 
 const  { extractValidFields } = require('../lib/validation')
+const bcrypt = require("bcryptjs");
+const { getDbReference } = require("../lib/mongo");
+const { ObjectId } = require('mongodb');
 
 LocationSchema = {
-      id: { required: true },
+      //id: { required: true },
       name: { required: true },
       desc: { required: true },
       population: { required: true },
       capacity: { required: true }
 };
-exports.LocationSchema = LocationSchema;
+exports.LocationSchema = LocationSchema
+
+
+async function getLocationsPage(page) {
+  const db = getDbReference();
+  const collection = db.collection('locations');
+
+  const count = await collection.countDocuments();
+  const pageSize = 10;
+  const lastPage = Math.ceil(count / pageSize);
+  page = page > lastPage ? lastPage : page;
+  page = page < 1 ? 1 : page;
+  const offset = (page - 1) * pageSize;
+
+  const results = await collection.find({})
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(pageSize)
+    .toArray();
+
+  return {
+    locations: results,
+    page: page,
+    totalPages: lastPage,
+    pageSize: pageSize,
+    count: count
+  };
+}
+exports.getLocationsPage = getLocationsPage;
+
+
+async function insertNewLocation(location) {
+  location = extractValidFields(location, LocationSchema);
+  const db = getDbReference();
+  const collection = db.collection('locations');
+  const result = await collection.insertOne(location);
+  console.log("  -- result:", result);
+  return result.insertedId;
+}
+exports.insertNewLocation = insertNewLocation;
+
+async function getLocationById(id){
+  const db = getDbReference();
+  const collection = db.collection('locations');
+  if (!ObjectId.isValid(id)) {
+    return null;
+  } else {
+    const results = await collection
+      .find({ _id: new ObjectId(id) })
+      .toArray();
+    return results[0];
+  }
+}
+exports.getLocationById = getLocationById;
+
+
+async function replaceLocationById(id, location){
+  const db = getDbReference();
+  const collection = db.collection('locations');
+  if (!ObjectId.isValid(id)) {
+		return null;
+	}
+	const exists = await collection.find({ _id: new ObjectId(id) }).limit(1).count(true);
+
+  if (!exists) {
+		return null;
+	}
+	await collection.replaceOne({ _id: new ObjectId(id) }, location );
+	return {};
+
+}
+exports.replaceLocationById = replaceLocationById;
+
+async function deleteLocationById(id){
+  const db = getDbReference();
+  const collection = db.collection('locations');
+  if (!ObjectId.isValid(id)) {
+		return null;
+	}
+	const exists = await collection.find({ _id: new ObjectId(id) }).limit(1).count(true);
+	if (!exists) {
+		return null;
+	}
+	await collection.deleteOne({ _id: new ObjectId(id) })
+	return {};
+}
+exports.deleteLocationById = deleteLocationById;
