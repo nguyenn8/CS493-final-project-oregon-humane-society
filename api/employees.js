4acf6fb2e740198e2c3ffc218de5d1a79d1b7981
Laura Jiang <jianglau@oregonstate.edu>
2021-06-06 19:08:23 -0700

    employees fix

--- api/employees.js
+++ api/employees.js
@@ -1,42 +1,58 @@
 const router = require("express").Router();
-const { DataSource } = require("apollo-datasource");
 const { getDbReference } = require("../lib/mongo");
 const { ObjectId } = require("mongodb");
 const { EmployeeSchema } = require("../models/employee");
+const { validateUser } = require("../lib/validation");
+const { insertNewEmployee } = require("../models/employee");
 
-class EmployeesAPI extends DataSource {
-  constructor() {
-    super();
-    this.db = getDbReference();
+router.post("/", async (req, res, next) => {
+  if (validateAgainstSchema(req.body, UserSchema)) {
+    try {
+      const id = await insertNewEmployee(req.body);
+      res.status(201).send({
+        _id: id,
+      });
+    } catch (err) {
+      console.error("  -- Error:", err);
+      res.status(500).send({
+        error: "Error inserting new user.  Try again later.",
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body does not contain a valid User.",
+    });
   }
+});
 
-  async getAllEmployees() {
-    const collection = this.db.collection("employees");
-    const results = await collection.find({}).toArray();
-    return results;
+/* Once you have enabled user registration for your application, implement a new POST /users/login API endpoint that allows a registered user to log in by sending their email address and password. If the email/password combination is valid, you should respond with a JWT token, which the user can then send with future requests to authenticate themselves. The JWT token payload should contain the user's ID (with which you should be able to fetch details about the user from the database) and any other information needed to implement the features described in this assignment, and it should expire after 24 hours.
+
+If a user attempts to log in with an invalid username or password, you should respond with a 401 error. */
+
+router.post("/login", async (req, res, next) => {
+  if (req.body && req.body.id && req.body.password) {
+    try {
+      const authenticated = await validateUser(req.body.id, req.body.password);
+      if (authenticated) {
+        res.status(200).send({
+          token: generateAuthToken(req.body.id),
+        });
+      } else {
+        res.status(401).send({
+          error: "Invalid authentication credentials.",
+        });
+      }
+    } catch (err) {
+      console.error("  -- error:", err);
+      res.status(500).send({
+        error: "Error logging in.  Try again later.",
+      });
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body needs `id` and `password`.",
+    });
   }
+});
 
-  async getEmployeeById({ employeeId }) {
-    console.log(employeeId);
-    const collection = this.db.collection("employees");
-    const results = await collection
-      .find({ _id: ObjectId(employeeId) })
-      .toArray();
-    console.log(results);
-    return results[0];
-  }
-
-  async addNewEmployee() {
-    employee = extractValidFields(employee, EmployeeSchema);
-    const collection = this.db.collection("employees");
-    const result = await collection.insertOne(employee);
-    return result.insertedId;
-  }
-
-  async employeeLogin() {}
-
-  async getEmployeeServicesById({ employeeId }) {}
-
-  async deleteEmployee() {}
-}
-module.exports = EmployeesAPI;
+module.exports = router;
--- models/employee.js
+++ models/employee.js
@@ -2,12 +2,87 @@
  * Employee schema and associated methods.
  */
 
-const  { extractValidFields } = require('../lib/validation')
+const mysqlPool = require("../lib/mysqlPool");
+const bcrypt = require("bcryptjs");
+
+const { extractValidFields } = require("../lib/validation");
+const { getDbReference } = require("../lib/mongo");
+
+const db = getDbReference();
+const collection = db.collection("employees");
+
+/*
+ * Schema for an employee.
+ */
 
 const EmployeeSchema = {
   id: { required: true },
   username: { required: true },
   password: { required: true },
-  date_hired: { required: true }
+  date_hired: { required: true },
 };
 exports.EmployeeSchema = EmployeeSchema;
+
+/*
+ * Insert a new employee into the DB.
+ */
+exports.insertNewEmployee = async function (user) {
+  const userToInsert = extractValidFields(user, UserSchema);
+  console.log("  -- userToInsert before hashing:", userToInsert);
+  userToInsert.password = await bcrypt.hash(userToInsert.password, 8);
+  console.log("  -- userToInsert after hashing:", userToInsert);
+  const [result] = await mysqlPool.query("INSERT INTO users SET ?", user);
+  return result.insertId;
+};
+
+/*
+ * Fetch an employee from the DB based on employee ID.
+ */
+async function getEmployeeById(id, includePassword) {
+  let results;
+  if (includePassword) {
+    [results] = await mysqlPool.query("SELECT * FROM users WHERE id = ?", id);
+  } else {
+    results = await mysqlPool.query(
+      "SELECT name, email, admin FROM users WHERE id = ?",
+      id
+    );
+  }
+  return results[0];
+}
+
+exports.getUserById = getUserById;
+
+exports.validateEmployee = async function (id, password) {
+  console.log(id, password);
+  const employee = await getUserById(id, true);
+  console.log(employee);
+  return user && (await bcrypt.compare(password, employee.password));
+};
+
+exports.getAllEmployees = async function () {
+  const collection = db.collection("employees");
+  const results = await collection.find({}).toArray();
+  return results;
+};
+
+exports.getEmployeeById = async function (employeeId) {
+  console.log(employeeId);
+  const collection = this.db.collection("employees");
+  const results = await collection
+    .find({ _id: ObjectId(employeeId) })
+    .toArray();
+  console.log(results);
+  return results[0];
+};
+
+exports.addNewEmployee = async function () {
+  employee = extractValidFields(employee, EmployeeSchema);
+  const collection = this.db.collection("employees");
+  const result = await collection.insertOne(employee);
+  return result.insertedId;
+};
+
+exports.getEmployeeServicesById = async function (employeeId) {};
+
+exports.deleteEmployee = async function () {};
