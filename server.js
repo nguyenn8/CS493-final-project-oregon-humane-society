20316a3ec1ae01726bcf09046b2e865ecdf53a43
Zach <taylozac@oregonstate.edu>
2021-06-06 18:54:44 -0700

    Abandon GraphQL. :(

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
-const { ApolloServer } = require("apollo-server-express");
-
-const typeDefs = require("./schemas/schema");
-const AnimalsAPI = require("./api/animals");
-const EmployeesAPI = require("./api/employees");
-const LocationsAPI = require("./api/locations");
-const ServicesAPI = require("./api/services");
-const resolvers = require("./resolvers")
 
 
 const { connectToDb, getDbReference } = require('./lib/mongo');
-//const api = require("./api");
+const api = require("./api");
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -28,32 +20,18 @@ app.use(express.static("public"));
  * All routes for the API are written in modules in the api/ directory.  The
  * top-level router lives in api/index.js.
  */
-//app.use("/", api);
+app.use("/", api);
 
-
-/*
- *
- */
-connectToDb(async () => {
-	const db = getDbReference();
-	const server = new ApolloServer({ 
-  	typeDefs,
-		resolvers,
-		dataSources: () => ({
-			animalsAPI: new AnimalsAPI(),
-			employeesAPI: new EmployeesAPI(),
-			locationsAPI: new LocationsAPI(),
-			servicesAPI: new ServicesAPI()
-		})
-	});
-	server.applyMiddleware({ app });
-
-	app.use("*", function (req, res, next) {
+app.use("*", function (req, res, next) {
   	res.status(404).json({
     	error: "Requested resource " + req.originalUrl + " does not exist",
 	  });
 	});
 
+/*
+ *
+ */
+connectToDb(async () => {
   app.listen(port, function () {
     console.log("== Server is running on port", port);
   });
